0579fab7f8ea5399214ed8896a27917d928ce800
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2021-10-26 17:28:52 -0400

    fix: add serviceUrl to status badges

--- src/components/RequestList/RequestItem/index.tsx
+++ src/components/RequestList/RequestItem/index.tsx
@@ -137,8 +137,18 @@ const RequestItemError: React.FC<RequestItemErrorProps> = ({
                       ).length > 0
                     }
                     is4k={requestData.is4k}
-                    plexUrl={requestData.media.plexUrl}
-                    plexUrl4k={requestData.media.plexUrl4k}
+                    plexUrl={
+                      requestData.is4k
+                        ? requestData.media.plexUrl4k
+                        : requestData.media.plexUrl
+                    }
+                    serviceUrl={
+                      hasPermission(Permission.ADMIN)
+                        ? requestData.is4k
+                          ? requestData.media.serviceUrl4k
+                          : requestData.media.serviceUrl
+                        : undefined
+                    }
                   />
                 )}
               </div>
